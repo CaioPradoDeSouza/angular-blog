db9d06c39f480c869deb484f6d7773ddb0857a80
Caio Prado <cpssouza7@gmail.com>
2025-05-06 16:45:30 -0300

    Feat: criado pages e home page

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuBarComponent, MenuTitleComponent],
+  imports: [RouterOutlet, MenuBarComponent, MenuTitleComponent, BigCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   
