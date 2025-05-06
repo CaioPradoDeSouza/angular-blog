18b1f75d34da687a37941751cf24d56b26ddd9ec
Caio Prado <cpssouza7@gmail.com>
2025-05-06 16:42:44 -0300

    Feat: criado estrutura menu-title

--- angular-blog/src/app/app.component.ts
+++ angular-blog/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
+
 })
 export class AppComponent {
   title = 'angular-blog';
